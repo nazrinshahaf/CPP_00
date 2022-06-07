d9efd488c672f1955cb7ac748f74ec003a108ed1
Nazrin shah Fernandez <nfernand@u82z06s01.42kl.edu.my>
2022-06-07 15:19:57 +0800

    changed ex00 from using char pointer to string

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,26 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfernand <nfernand@student.42kl.edu.m      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/07 15:18:16 by nfernand          #+#    #+#             */
+/*   Updated: 2022/06/07 15:19:22 by nfernand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
+#include <string>
 #include <cctype>
 
 using std::cout;
 using std::endl;
+using std::string;
 
 int	main(int argc, char **argv)
 {
-	int i;
-	int y;
+	int 	i;
+	string	str;
 
 	i = 1;
 	if (argc > 1)
 	{
 		while (argv[i])
 		{
-			y = 0;
-			while (argv[i][y])
-			{
-				argv[i][y] = (char)std::toupper(argv[i][y]);
-				y++;
-			}
-			cout << argv[i];
+			str = argv[i];
+			std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+			cout << str;
 			i++;
 		}
 		cout << endl;
